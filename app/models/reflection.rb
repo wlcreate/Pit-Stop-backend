a8659c0b9da280ac99c790e3d14ee0f7516ec17b
wlcreate <waverley.nyc@gmail.com>
2020-11-10 18:55:37 -0500

    adds seed data

--- app/models/reflection.rb
+++ app/models/reflection.rb
@@ -1,6 +1,11 @@
 class Reflection < ApplicationRecord
   belongs_to :place
 
+  validates :date_visited, presence: true
+  validates :rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10 } 
+  # validates :rating, presence: true, numericality: { only_integer: true }, inclusion: {in: 0..10, message: "needs to be between 0 and 10" }
+  validates :content, presence: true
+
   def nice_timestamp
     self.date_visited.strftime("%m/%d/%Y")
   end
